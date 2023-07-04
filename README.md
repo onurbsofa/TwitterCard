10091e1a66615f6277a95738fd4115b9b815f0bb
Bruno Fazio <brunofazio466@gmail.com>
2023-07-03 23:01:21 -0300

    array de usuarios y ruta para imagen en readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # TwitterCard
 componente para react con props 
-![TewitterCards](public\img\screencapture-localhost-5173-2023-07-02-20_33_40.png)
+![TewitterCards](
+https://user-images.githubusercontent.com/68760595/128637245-5b0b5b7a-4b9a-4b0d-8b9a-2b5b0b2b2b2b.png
+)
+)
--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,40 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 export function App (){
     //el esatdo inicial es una prop del componente que no cambia si modificamos el estado del componente padre , en este caso el componente padre es App.jsx y el hijo es TwitterFollowCard.jsx.
     //Siempre que se modifique el estado del componente padre se renderiza el componente hijo con el estado inicial ya definido, no cambiara porque el valor se lo damos inicialmente al hijo no al padre
-    const miduProps = { userName: "midudev"}// Objeto con las props para pasar al componente
+   //Objeto con las props para pasar al componente
+
+    const users = [
+        {
+            userName: "midudev",
+            name: "Miguel Ángel Durán",
+            isFollowed: true
+        },
+        {
+            userName: "bofoweb",
+            name: "bruno",
+            isFollowed: false
+        },
+        {
+            userName: "Reddit",
+            name: "Robotito",
+            isFollowed: true
+        },
+    ]
     return(
         <section className='App'>
-            <TwitterFollowCard    
-            userName= "bofoweb"
-            initialIsFollowed={true}//valor inicial del estado del componente para que sea dinamico asi funciona twitter
-            > 
-            Bruno
-            </TwitterFollowCard>
-            <TwitterFollowCard 
-            userName= "Reddit"
-            >
-            Robotito
-            </TwitterFollowCard>
-            <TwitterFollowCard
-            {...miduProps} // rest operator en las props malas practicas pero aveces es necesario
-            >
-            MiduLove
-            </TwitterFollowCard>
+           {
+            users.map( user =>{
+                const {userName, name, isFollowed} = user
+                return(
+                    <TwitterFollowCard
+                        key={userName}//identificador unico para cada componente porque es un array
+                        userName={userName}
+                        initialIsFollowed={isFollowed}>
+                        {name}
+                    </TwitterFollowCard>
+                )
+            })
+           }
         </section>
     )
 }
